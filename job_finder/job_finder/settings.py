fee32f71309816733c9db402a7bf5b639ec6a16c
Taranjeet Singh <singhtaran1515@gmail.com>
2020-05-22 21:38:10 +0530

    Bugs Fixed

--- job_finder/job_finder/settings.py
+++ job_finder/job_finder/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '-0l(*ul4^5)d(#gy9_%_jpw2j$(#%ko*u*h*dr8%_bz)nrvn)y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
